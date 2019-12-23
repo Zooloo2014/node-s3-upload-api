93317c00446348a0be00ba00945bf63ec0cce26e
Brent Engelbrecht <brentengelbrecht@wfs.co.za>
2019-12-23 21:25:16 +0200

    Add a repository layer

---
+++ src/datastore/repository.js
@@ -0,0 +1,12 @@
+
+function saveUserDetails() {
+}
+
+function getUserDetails() {
+}
+
+function authenticateUser(username, password) {
+  return { username: username.toLowerCase() }
+}
+
+module.exports = { saveUserDetails, getUserDetails, authenticateUser }
--- src/routes/users.js
+++ src/routes/users.js
@@ -1,16 +1,38 @@
 const jwt = require('jsonwebtoken')
+const repository = require('../datastore/repository')
+
+function register (ctx) {
+  const {
+    firstName,
+    lastName,
+    userName,
+    password,
+    email
+  } = ctx.request
+
+  repository.saveUserDetails({
+    firstName,
+    lastName,
+    userName,
+    password,
+    email
+  })
+}
 
 function login (ctx) {
   const { username, password } = ctx.request.body
   if (!username) ctx.throw(422, 'Username required.')
   if (!password) ctx.throw(422, 'Password required.')
 
-  // TODO Check that user is bona fide
+  const userDetails = repository.authenticateUser(username, password) 
+  if (!userDetails) {
+    ctx.throw(err.status || 401, err.text)
+  }
 
-  const payload = { sub: username }
+  const payload = { sub: userDetails.username }
   const secret = process.env.JWT_SECRET || 'secret'
   const token = jwt.sign(payload, secret);
   ctx.body = token
 }
 
-module.exports = { login }
+module.exports = { register, login }
